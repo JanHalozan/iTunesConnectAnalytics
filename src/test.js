b1faf49a8516fc498bd878558b7807de08f10306
Marko <marko@ip-192-168-1-134.eu-central-1.compute.internal>
2016-05-11 10:25:40 +0200

    test website sources query

--- src/test.js
+++ src/test.js
@@ -1,12 +1,14 @@
 var itc = require('./analytics.js');
 var Itunes = itc.Itunes;
 var Query = itc.Query;
+var Report = itc.Report;
 
 var instance = new Itunes('UNAME', 'PASS');
 
-var query = new Query('940584421', {
-  measures: itc.measures.pageViews
-});
+var query = Report.sources('940584421', {
+  measures:  itc.measures.pageViews,
+  dimension: itc.dimension.websites
+}).date('2016-05-03','2016-05-10');
 
 instance.request(query, function(error, result) {
   console.log(JSON.stringify(result));
