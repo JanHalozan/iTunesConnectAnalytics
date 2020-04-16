d4b75ea1a3cc814387c6fd706bdaad58ad20b312
Shoma Okamoto <shoma.develop@gmail.com>
2020-04-16 13:38:48 +0900

    Delete the duplicate line.

--- src/analytics.js
+++ src/analytics.js
@@ -236,5 +236,4 @@ module.exports.measures = query.measures;
 module.exports.dimension = query.dimension;
 module.exports.dimensionFilterKey = query.dimensionFilterKey;
 module.exports.platform = query.platform;
-module.exports.frequency = query.frequency;
 module.exports.queryType = query.queryType;
